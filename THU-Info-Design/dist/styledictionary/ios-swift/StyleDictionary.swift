3a937e6c92fb44d287cf3aa624ac6d9642b5d3a1
JohnnyBGoodeLithium <1571322820@qq.com>
2022-06-12 17:58:58 +0800

    重置 Design Tocken 源文件，录入实际颜色库并创建与业务联系的快捷颜色映射

---
+++ THU-Info-Design/dist/styledictionary/ios-swift/StyleDictionary.swift
@@ -0,0 +1,50 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Sun, 12 Jun 2022 09:49:46 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let infoDsAliasTranscriptA = UIColor(red: 0.345, green: 0.337, blue: 0.839, alpha: 1)
+    public static let infoDsAliasTranscriptAMinus = UIColor(red: 0.039, green: 0.518, blue: 1.000, alpha: 1)
+    public static let infoDsAliasTranscriptAPlus = UIColor(red: 0.545, green: 0.333, blue: 0.894, alpha: 1)
+    public static let infoDsAliasTranscriptB = UIColor(red: 0.000, green: 0.780, blue: 0.745, alpha: 1)
+    public static let infoDsAliasTranscriptBMinus = UIColor(red: 0.204, green: 0.780, blue: 0.349, alpha: 1)
+    public static let infoDsAliasTranscriptBPlus = UIColor(red: 0.196, green: 0.678, blue: 0.902, alpha: 1)
+    public static let infoDsAliasTranscriptC = UIColor(red: 1.000, green: 0.800, blue: 0.000, alpha: 1)
+    public static let infoDsAliasTranscriptCMinus = UIColor(red: 1.000, green: 0.584, blue: 0.000, alpha: 1)
+    public static let infoDsAliasTranscriptCPlus = UIColor(red: 0.596, green: 0.780, blue: 0.204, alpha: 1)
+    public static let infoDsAliasTranscriptD = UIColor(red: 0.871, green: 0.592, blue: 0.055, alpha: 1)
+    public static let infoDsAliasTranscriptDPlus = UIColor(red: 0.722, green: 0.514, blue: 0.110, alpha: 1)
+    public static let infoDsAliasTranscriptEx = UIColor(red: 0.000, green: 0.780, blue: 0.745, alpha: 1)
+    public static let infoDsAliasTranscriptF = UIColor(red: 1.000, green: 0.231, blue: 0.188, alpha: 0.6)
+    public static let infoDsAliasTranscriptI = UIColor(red: 1.000, green: 0.231, blue: 0.188, alpha: 0.6)
+    public static let infoDsAliasTranscriptP = UIColor(red: 0.545, green: 0.333, blue: 0.894, alpha: 1)
+    public static let infoDsAliasTranscriptW = UIColor(red: 0.780, green: 0.780, blue: 0.800, alpha: 1)
+    public static let infoDsColorLightBgTextBlack0 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let infoDsColorLightBgTextBlack3 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let infoDsColorLightBgTextBlack6 = UIColor(red: 0.400, green: 0.400, blue: 0.400, alpha: 1)
+    public static let infoDsColorLightBgTextBlack9 = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let infoDsColorLightBgTextWhiteC7 = UIColor(red: 0.780, green: 0.780, blue: 0.800, alpha: 1)
+    public static let infoDsColorLightBgTextWhiteF7 = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
+    public static let infoDsColorLightBgTextWhiteFf = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let infoDsColorLightFuncBlue = UIColor(red: 0.039, green: 0.518, blue: 1.000, alpha: 1)
+    public static let infoDsColorLightFuncGreen = UIColor(red: 0.204, green: 0.780, blue: 0.349, alpha: 1)
+    public static let infoDsColorLightFuncRed = UIColor(red: 1.000, green: 0.231, blue: 0.188, alpha: 1)
+    public static let infoDsColorLightFuncSecBrown1 = UIColor(red: 0.871, green: 0.592, blue: 0.055, alpha: 1)
+    public static let infoDsColorLightFuncSecBrown2 = UIColor(red: 0.722, green: 0.514, blue: 0.110, alpha: 1)
+    public static let infoDsColorLightFuncSecCyan = UIColor(red: 0.196, green: 0.678, blue: 0.902, alpha: 1)
+    public static let infoDsColorLightFuncSecGrass = UIColor(red: 0.596, green: 0.780, blue: 0.204, alpha: 1)
+    public static let infoDsColorLightFuncSecIndigo = UIColor(red: 0.345, green: 0.337, blue: 0.839, alpha: 1)
+    public static let infoDsColorLightFuncSecMint = UIColor(red: 0.000, green: 0.780, blue: 0.745, alpha: 1)
+    public static let infoDsColorLightFuncSecOrange = UIColor(red: 1.000, green: 0.584, blue: 0.000, alpha: 1)
+    public static let infoDsColorLightFuncSecRed60 = UIColor(red: 1.000, green: 0.231, blue: 0.188, alpha: 0.6)
+    public static let infoDsColorLightFuncYellow = UIColor(red: 1.000, green: 0.800, blue: 0.000, alpha: 1)
+    public static let infoDsColorLightPrimary1 = UIColor(red: 0.545, green: 0.333, blue: 0.894, alpha: 1)
+    public static let infoDsColorLightPrimary2 = UIColor(red: 0.722, green: 0.565, blue: 0.980, alpha: 1)
+}
---
+++ THU-Info-Design/dist/styledictionary/js/tokens.js
@@ -0,0 +1,42 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 12 Jun 2022 09:49:46 GMT
+ */
+
+export const InfoDsAliasTranscriptAPlus = "#8b55e4";
+export const InfoDsAliasTranscriptA = "#5856d6";
+export const InfoDsAliasTranscriptAMinus = "#0a84ff";
+export const InfoDsAliasTranscriptBPlus = "#32ade6";
+export const InfoDsAliasTranscriptB = "#00c7be";
+export const InfoDsAliasTranscriptBMinus = "#34c759";
+export const InfoDsAliasTranscriptCPlus = "#98c734";
+export const InfoDsAliasTranscriptC = "#ffcc00";
+export const InfoDsAliasTranscriptCMinus = "#ff9500";
+export const InfoDsAliasTranscriptDPlus = "#b8831c";
+export const InfoDsAliasTranscriptD = "#de970e";
+export const InfoDsAliasTranscriptF = "#ff3b30";
+export const InfoDsAliasTranscriptI = "#ff3b30";
+export const InfoDsAliasTranscriptP = "#8b55e4";
+export const InfoDsAliasTranscriptEx = "#00c7be";
+export const InfoDsAliasTranscriptW = "#c7c7cc";
+export const InfoDsColorLightPrimary1 = "#8b55e4";
+export const InfoDsColorLightPrimary2 = "#b890fa";
+export const InfoDsColorLightFuncRed = "#ff3b30";
+export const InfoDsColorLightFuncGreen = "#34c759";
+export const InfoDsColorLightFuncYellow = "#ffcc00";
+export const InfoDsColorLightFuncBlue = "#0a84ff";
+export const InfoDsColorLightBgTextBlack0 = "#000000";
+export const InfoDsColorLightBgTextBlack3 = "#333333";
+export const InfoDsColorLightBgTextBlack6 = "#666666";
+export const InfoDsColorLightBgTextBlack9 = "#999999";
+export const InfoDsColorLightBgTextWhiteFf = "#ffffff";
+export const InfoDsColorLightBgTextWhiteF7 = "#f7f7f7";
+export const InfoDsColorLightBgTextWhiteC7 = "#c7c7cc";
+export const InfoDsColorLightFuncSecIndigo = "#5856d6";
+export const InfoDsColorLightFuncSecMint = "#00c7be";
+export const InfoDsColorLightFuncSecGrass = "#98c734";
+export const InfoDsColorLightFuncSecOrange = "#ff9500";
+export const InfoDsColorLightFuncSecBrown1 = "#de970e";
+export const InfoDsColorLightFuncSecBrown2 = "#b8831c";
+export const InfoDsColorLightFuncSecRed60 = "#ff3b30";
+export const InfoDsColorLightFuncSecCyan = "#32ade6";
